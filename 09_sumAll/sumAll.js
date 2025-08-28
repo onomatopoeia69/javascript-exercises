3de2f7dca3ba43d382879990cd909b7f69193ff5
onomatopoeia69 <carljameslangres@gmail.com>
2025-08-28 18:23:29 +0800

    sumAll success

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+   let arr = [];
+      let NewStart = start < end ? start : end;
+      let NewEnd = end > start ? end : start;
+      
+      
+     if (Number.isInteger(NewStart) && Number.isInteger(NewEnd) && NewStart > 0 && NewEnd > 0)
+    {
+
+    for (let index = NewStart; index <= NewEnd; index++) {
+        arr.push(index);  
+    }
+    
+    }else{
+        
+        return "ERROR";
+        
+    }
+
+    return arr.reduce((accu, curr) => accu + curr , 0);
 
 };
 
