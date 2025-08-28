cefff4608d1d801443c94ab43834e684ab02a74f
onomatopoeia69 <carljameslangres@gmail.com>
2025-08-28 15:58:53 +0800

    remove arrayfromarray success

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...array) {
+
+   let newArr = [];
+        
+    for (let i = 0 ; i < arr.length; i++)
+        {
+            
+            if( array.includes(arr[i]))
+            {
+                continue;
+            }else{
+                newArr.push(arr[i]);   
+            }
+        }
+    
+    return newArr;
+
+
 };
 
 // Do not edit below this line
