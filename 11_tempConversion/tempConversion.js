da4f855354b14107be94876c8ed37fdadf13c08d
onomatopoeia69 <carljameslangres@gmail.com>
2025-08-28 22:26:37 +0800

    tempConversion success

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+
+
+    return +((fahr - 32 )*(5/9)).toFixed(1);
+   
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+
+
+  
+  return +(cels * (9/5) + 32).toFixed(1);
+
 };
 
 // Do not edit below this line
