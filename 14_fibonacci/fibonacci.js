5f5abceaf599c94731442b59aaa567198712ba50
onomatopoeia69 <carljameslangres@gmail.com>
2025-09-29 18:09:56 +0800

    fibonacci complete

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+       
+    let collect = 0;
+    let store = [];
+    
+    if(num < 0 ){
+        
+        return "OOPS";
+    }
+    
+    for (index = 0; index <= +num; index++)
+    {
+          
+         if(store.length > 1)
+         {
+           collect = store[store.length-1] + store[store.length-2];
+           store.push(collect);
+         }else{
+             collect += index;
+            store.push(collect); 
+         }
+          
+    }
+    
+    return  store[store.length-1];
+    
+
 
 };
 
