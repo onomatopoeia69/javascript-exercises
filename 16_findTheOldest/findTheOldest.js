868e420fbdd327b8643029b75370b52af8928b20
onomatopoeia69 <carljameslangres@gmail.com>
2025-09-30 00:26:35 +0800

    findTheOldest complete

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+   
+       let oldList = Object.values(people).map((values) => {
+            return  [
+                    {
+                        name: values.name,
+                        age: (values.yearOfDeath ?? new Date().getFullYear()) - values.yearOfBirth , 
+                    }
+                ]
+        }).flat().filter((values) => values.age > 0).sort((a,b) => b.age -a.age);
+           
+       
+        return oldList[0];
 
 };
 
