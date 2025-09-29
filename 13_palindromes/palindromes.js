4d8713c99766a9e313c8da22eca9a71f38d564bf
onomatopoeia69 <carljameslangres@gmail.com>
2025-09-29 13:50:21 +0800

    palindromes complete

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let str1 =  string.toLowerCase().replace(/[^a-zA-Z0-9]/g,"");
+    let str2 = string.toLowerCase().replace(/[^a-zA-Z0-9]/g,"").split("").reverse().join("");
+    
+    return str1 == str2;
 };
 
 // Do not edit below this line
